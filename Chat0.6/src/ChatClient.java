e9d90c09d050b28eb64fcc58d06b7a7497e2848b
Tianwu <2652017679@qq.com>
2016-09-26 20:21:18 +0800

    V6.0
    开启服务器，开启客户端，两者能连上

---
+++ Chat0.6/src/ChatClient.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.*;
+
+public class ChatClient extends Frame{
+	TextField tfTxt = new TextField();
+	TextArea taContent = new TextArea();
+	public static void main(String[] args) {
+		new ChatClient().launchFrame();;
+
+	}
+	public void launchFrame() {
+		this.setTitle("Java在线聊天系统");
+		this.setLocation(400, 300);
+		this.setSize(300, 300);
+		this.add(tfTxt, BorderLayout.SOUTH);
+		this.add(taContent, BorderLayout.NORTH);
+		this.pack();
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);;
+			}
+		});
+		tfTxt.addActionListener(new TFListener());
+		this.setVisible(true);
+		
+		connect();
+	}
+	public void connect() {
+		try {
+			Socket s = new Socket("127.0.0.1", 8888);
+System.out.println("Connected!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//函数是接口
+	private class TFListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			String s = tfTxt.getText().trim();
+			taContent.setText(s);
+			tfTxt.setText("");
+		} 
+		
+	}
+}
