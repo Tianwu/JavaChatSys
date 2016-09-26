30f257ff01da78b6208c549a16f818b8b562467b
Tianwu <2652017679@qq.com>
2016-09-26 20:19:46 +0800

    V3.0
    设置窗口关闭形式（点击×，关闭窗体）
    匿名类重写 windowClosing方法

---
+++ Chat0.3/src/ChatClient.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ChatClient extends Frame{
+	TextField tfTxt = new TextField();
+	TextArea taContent = new TextArea();
+	public static void main(String[] args) {
+		new ChatClient().launchFrame();;
+
+	}
+	public void launchFrame() {
+		this.setTitle("Java在线聊天系统");
+		this.setLocation(400, 300);
+		this.setSize(300, 300);
+		this.add(tfTxt, BorderLayout.SOUTH);
+		this.add(taContent, BorderLayout.NORTH);
+		this.pack();
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);;
+			}
+		});
+		this.setVisible(true);
+	}
+	
+}
