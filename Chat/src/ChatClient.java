9fbd0d033f8eec2c298a8d5ef3d31a17da539da2
Tianwu <2652017679@qq.com>
2016-09-26 20:18:19 +0800

    V1.0
    创建一个窗口，起一个客户端

---
+++ Chat/src/ChatClient.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class ChatClient extends Frame{
+
+	public static void main(String[] args) {
+		new ChatClient().launchFrame();;
+
+	}
+	public void launchFrame() {
+		this.setLocation(400, 300);
+		this.setSize(300, 300);
+		this.setVisible(true);
+	}
+}
